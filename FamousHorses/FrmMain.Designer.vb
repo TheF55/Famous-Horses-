f032a79ff129af3eed11410e3702e19db4ef4216
TheF55 <162068579+TheF55@users.noreply.github.com>
2024-03-03 19:12:16 -0600

    Add files via upload

---
+++ FamousHorses/FrmMain.Designer.vb
@@ -0,0 +1,347 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim HorseLabel As System.Windows.Forms.Label
+        Dim RiderLabel As System.Windows.Forms.Label
+        Dim SeriesLabel As System.Windows.Forms.Label
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmMain))
+        Dim LblLookup As System.Windows.Forms.Label
+        Me.LblMain = New System.Windows.Forms.Label()
+        Me.HorsesDataSet = New FamousHorses.horsesDataSet()
+        Me.TblHorsesBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.TblHorsesTableAdapter = New FamousHorses.horsesDataSetTableAdapters.tblHorsesTableAdapter()
+        Me.TableAdapterManager = New FamousHorses.horsesDataSetTableAdapters.TableAdapterManager()
+        Me.HorseTextBox = New System.Windows.Forms.TextBox()
+        Me.RiderTextBox = New System.Windows.Forms.TextBox()
+        Me.SeriesTextBox = New System.Windows.Forms.TextBox()
+        Me.BtnFirst = New System.Windows.Forms.Button()
+        Me.BtnLast = New System.Windows.Forms.Button()
+        Me.BtnPrevious = New System.Windows.Forms.Button()
+        Me.BtnNext = New System.Windows.Forms.Button()
+        Me.BtnAdd = New System.Windows.Forms.Button()
+        Me.BtnSave = New System.Windows.Forms.Button()
+        Me.BtnPrint = New System.Windows.Forms.Button()
+        Me.BtnDelete = New System.Windows.Forms.Button()
+        Me.BtnEdit = New System.Windows.Forms.Button()
+        Me.PrtDocument = New System.Drawing.Printing.PrintDocument()
+        Me.PpdPreview = New System.Windows.Forms.PrintPreviewDialog()
+        Me.BtnDeleteAll = New System.Windows.Forms.Button()
+        Me.TxtLookup = New System.Windows.Forms.TextBox()
+        Me.BtnLookup = New System.Windows.Forms.Button()
+        HorseLabel = New System.Windows.Forms.Label()
+        RiderLabel = New System.Windows.Forms.Label()
+        SeriesLabel = New System.Windows.Forms.Label()
+        LblLookup = New System.Windows.Forms.Label()
+        CType(Me.HorsesDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TblHorsesBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'HorseLabel
+        '
+        HorseLabel.AutoSize = True
+        HorseLabel.Location = New System.Drawing.Point(371, 180)
+        HorseLabel.Name = "HorseLabel"
+        HorseLabel.Size = New System.Drawing.Size(38, 13)
+        HorseLabel.TabIndex = 10
+        HorseLabel.Text = "Horse:"
+        '
+        'RiderLabel
+        '
+        RiderLabel.AutoSize = True
+        RiderLabel.Location = New System.Drawing.Point(371, 206)
+        RiderLabel.Name = "RiderLabel"
+        RiderLabel.Size = New System.Drawing.Size(35, 13)
+        RiderLabel.TabIndex = 12
+        RiderLabel.Text = "Rider:"
+        '
+        'SeriesLabel
+        '
+        SeriesLabel.AutoSize = True
+        SeriesLabel.Location = New System.Drawing.Point(371, 232)
+        SeriesLabel.Name = "SeriesLabel"
+        SeriesLabel.Size = New System.Drawing.Size(39, 13)
+        SeriesLabel.TabIndex = 14
+        SeriesLabel.Text = "Series:"
+        '
+        'LblMain
+        '
+        Me.LblMain.BackColor = System.Drawing.Color.PaleGreen
+        Me.LblMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.LblMain.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblMain.Location = New System.Drawing.Point(266, 75)
+        Me.LblMain.Name = "LblMain"
+        Me.LblMain.Size = New System.Drawing.Size(345, 53)
+        Me.LblMain.TabIndex = 9
+        Me.LblMain.Text = "Famous Horses"
+        Me.LblMain.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'HorsesDataSet
+        '
+        Me.HorsesDataSet.DataSetName = "horsesDataSet"
+        Me.HorsesDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'TblHorsesBindingSource
+        '
+        Me.TblHorsesBindingSource.DataMember = "tblHorses"
+        Me.TblHorsesBindingSource.DataSource = Me.HorsesDataSet
+        '
+        'TblHorsesTableAdapter
+        '
+        Me.TblHorsesTableAdapter.ClearBeforeFill = True
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
+        Me.TableAdapterManager.tblHorsesTableAdapter = Me.TblHorsesTableAdapter
+        Me.TableAdapterManager.UpdateOrder = FamousHorses.horsesDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        '
+        'HorseTextBox
+        '
+        Me.HorseTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.TblHorsesBindingSource, "Horse", True))
+        Me.HorseTextBox.Location = New System.Drawing.Point(416, 177)
+        Me.HorseTextBox.MaxLength = 10
+        Me.HorseTextBox.Name = "HorseTextBox"
+        Me.HorseTextBox.ReadOnly = True
+        Me.HorseTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.HorseTextBox.TabIndex = 11
+        '
+        'RiderTextBox
+        '
+        Me.RiderTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.TblHorsesBindingSource, "Rider", True))
+        Me.RiderTextBox.Location = New System.Drawing.Point(416, 203)
+        Me.RiderTextBox.MaxLength = 20
+        Me.RiderTextBox.Name = "RiderTextBox"
+        Me.RiderTextBox.ReadOnly = True
+        Me.RiderTextBox.Size = New System.Drawing.Size(159, 20)
+        Me.RiderTextBox.TabIndex = 13
+        '
+        'SeriesTextBox
+        '
+        Me.SeriesTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.TblHorsesBindingSource, "Series", True))
+        Me.SeriesTextBox.Location = New System.Drawing.Point(416, 229)
+        Me.SeriesTextBox.MaxLength = 25
+        Me.SeriesTextBox.Name = "SeriesTextBox"
+        Me.SeriesTextBox.ReadOnly = True
+        Me.SeriesTextBox.Size = New System.Drawing.Size(182, 20)
+        Me.SeriesTextBox.TabIndex = 15
+        '
+        'BtnFirst
+        '
+        Me.BtnFirst.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.BtnFirst.Image = Global.FamousHorses.My.Resources.Resources.First
+        Me.BtnFirst.Location = New System.Drawing.Point(369, 267)
+        Me.BtnFirst.Name = "BtnFirst"
+        Me.BtnFirst.Size = New System.Drawing.Size(43, 23)
+        Me.BtnFirst.TabIndex = 19
+        Me.BtnFirst.UseVisualStyleBackColor = False
+        '
+        'BtnLast
+        '
+        Me.BtnLast.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.BtnLast.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.BtnLast.Image = Global.FamousHorses.My.Resources.Resources.Last
+        Me.BtnLast.Location = New System.Drawing.Point(558, 267)
+        Me.BtnLast.Name = "BtnLast"
+        Me.BtnLast.Size = New System.Drawing.Size(43, 23)
+        Me.BtnLast.TabIndex = 18
+        Me.BtnLast.UseVisualStyleBackColor = False
+        '
+        'BtnPrevious
+        '
+        Me.BtnPrevious.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.BtnPrevious.Image = Global.FamousHorses.My.Resources.Resources.Previous
+        Me.BtnPrevious.Location = New System.Drawing.Point(432, 267)
+        Me.BtnPrevious.Name = "BtnPrevious"
+        Me.BtnPrevious.Size = New System.Drawing.Size(43, 23)
+        Me.BtnPrevious.TabIndex = 17
+        Me.BtnPrevious.UseVisualStyleBackColor = False
+        '
+        'BtnNext
+        '
+        Me.BtnNext.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.BtnNext.Image = Global.FamousHorses.My.Resources.Resources._Next
+        Me.BtnNext.Location = New System.Drawing.Point(495, 267)
+        Me.BtnNext.Name = "BtnNext"
+        Me.BtnNext.Size = New System.Drawing.Size(43, 23)
+        Me.BtnNext.TabIndex = 16
+        Me.BtnNext.UseVisualStyleBackColor = False
+        '
+        'BtnAdd
+        '
+        Me.BtnAdd.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.BtnAdd.Location = New System.Drawing.Point(167, 158)
+        Me.BtnAdd.Name = "BtnAdd"
+        Me.BtnAdd.Size = New System.Drawing.Size(70, 34)
+        Me.BtnAdd.TabIndex = 20
+        Me.BtnAdd.Text = "Add"
+        Me.BtnAdd.UseVisualStyleBackColor = False
+        '
+        'BtnSave
+        '
+        Me.BtnSave.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.BtnSave.Enabled = False
+        Me.BtnSave.Location = New System.Drawing.Point(693, 158)
+        Me.BtnSave.Name = "BtnSave"
+        Me.BtnSave.Size = New System.Drawing.Size(70, 34)
+        Me.BtnSave.TabIndex = 21
+        Me.BtnSave.Text = "Save"
+        Me.BtnSave.UseVisualStyleBackColor = False
+        '
+        'BtnPrint
+        '
+        Me.BtnPrint.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.BtnPrint.Location = New System.Drawing.Point(167, 364)
+        Me.BtnPrint.Name = "BtnPrint"
+        Me.BtnPrint.Size = New System.Drawing.Size(70, 34)
+        Me.BtnPrint.TabIndex = 22
+        Me.BtnPrint.Text = "Print"
+        Me.BtnPrint.UseVisualStyleBackColor = False
+        '
+        'BtnDelete
+        '
+        Me.BtnDelete.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.BtnDelete.Location = New System.Drawing.Point(167, 266)
+        Me.BtnDelete.Name = "BtnDelete"
+        Me.BtnDelete.Size = New System.Drawing.Size(70, 34)
+        Me.BtnDelete.TabIndex = 23
+        Me.BtnDelete.Text = "Delete"
+        Me.BtnDelete.UseVisualStyleBackColor = False
+        '
+        'BtnEdit
+        '
+        Me.BtnEdit.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.BtnEdit.Location = New System.Drawing.Point(167, 212)
+        Me.BtnEdit.Name = "BtnEdit"
+        Me.BtnEdit.Size = New System.Drawing.Size(70, 34)
+        Me.BtnEdit.TabIndex = 24
+        Me.BtnEdit.Text = "Edit"
+        Me.BtnEdit.UseVisualStyleBackColor = False
+        '
+        'PrtDocument
+        '
+        '
+        'PpdPreview
+        '
+        Me.PpdPreview.AutoScrollMargin = New System.Drawing.Size(0, 0)
+        Me.PpdPreview.AutoScrollMinSize = New System.Drawing.Size(0, 0)
+        Me.PpdPreview.ClientSize = New System.Drawing.Size(400, 300)
+        Me.PpdPreview.Enabled = True
+        Me.PpdPreview.Icon = CType(resources.GetObject("PpdPreview.Icon"), System.Drawing.Icon)
+        Me.PpdPreview.Name = "PpdPreview"
+        Me.PpdPreview.Visible = False
+        '
+        'BtnDeleteAll
+        '
+        Me.BtnDeleteAll.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.BtnDeleteAll.Location = New System.Drawing.Point(693, 221)
+        Me.BtnDeleteAll.Name = "BtnDeleteAll"
+        Me.BtnDeleteAll.Size = New System.Drawing.Size(70, 34)
+        Me.BtnDeleteAll.TabIndex = 25
+        Me.BtnDeleteAll.Text = "Delete All"
+        Me.BtnDeleteAll.UseVisualStyleBackColor = False
+        '
+        'TxtLookup
+        '
+        Me.TxtLookup.Location = New System.Drawing.Point(416, 326)
+        Me.TxtLookup.Name = "TxtLookup"
+        Me.TxtLookup.Size = New System.Drawing.Size(100, 20)
+        Me.TxtLookup.TabIndex = 26
+        '
+        'BtnLookup
+        '
+        Me.BtnLookup.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.BtnLookup.Location = New System.Drawing.Point(541, 318)
+        Me.BtnLookup.Name = "BtnLookup"
+        Me.BtnLookup.Size = New System.Drawing.Size(70, 34)
+        Me.BtnLookup.TabIndex = 27
+        Me.BtnLookup.Text = "Lookup"
+        Me.BtnLookup.UseVisualStyleBackColor = False
+        '
+        'LblLookup
+        '
+        LblLookup.AutoSize = True
+        LblLookup.Location = New System.Drawing.Point(299, 329)
+        LblLookup.Name = "LblLookup"
+        LblLookup.Size = New System.Drawing.Size(107, 13)
+        LblLookup.TabIndex = 28
+        LblLookup.Text = "Enter A Horse Name:"
+        '
+        'FrmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(930, 528)
+        Me.Controls.Add(LblLookup)
+        Me.Controls.Add(Me.BtnLookup)
+        Me.Controls.Add(Me.TxtLookup)
+        Me.Controls.Add(Me.BtnDeleteAll)
+        Me.Controls.Add(Me.BtnEdit)
+        Me.Controls.Add(Me.BtnDelete)
+        Me.Controls.Add(Me.BtnPrint)
+        Me.Controls.Add(Me.BtnSave)
+        Me.Controls.Add(Me.BtnAdd)
+        Me.Controls.Add(Me.BtnFirst)
+        Me.Controls.Add(Me.BtnLast)
+        Me.Controls.Add(Me.BtnPrevious)
+        Me.Controls.Add(Me.BtnNext)
+        Me.Controls.Add(HorseLabel)
+        Me.Controls.Add(Me.HorseTextBox)
+        Me.Controls.Add(RiderLabel)
+        Me.Controls.Add(Me.RiderTextBox)
+        Me.Controls.Add(SeriesLabel)
+        Me.Controls.Add(Me.SeriesTextBox)
+        Me.Controls.Add(Me.LblMain)
+        Me.Name = "FrmMain"
+        Me.Text = "Famours Horses"
+        CType(Me.HorsesDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TblHorsesBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents LblMain As Label
+    Friend WithEvents HorsesDataSet As horsesDataSet
+    Friend WithEvents TblHorsesBindingSource As BindingSource
+    Friend WithEvents TblHorsesTableAdapter As horsesDataSetTableAdapters.tblHorsesTableAdapter
+    Friend WithEvents TableAdapterManager As horsesDataSetTableAdapters.TableAdapterManager
+    Friend WithEvents HorseTextBox As TextBox
+    Friend WithEvents RiderTextBox As TextBox
+    Friend WithEvents SeriesTextBox As TextBox
+    Friend WithEvents BtnNext As Button
+    Friend WithEvents BtnPrevious As Button
+    Friend WithEvents BtnLast As Button
+    Friend WithEvents BtnFirst As Button
+    Friend WithEvents BtnAdd As Button
+    Friend WithEvents BtnSave As Button
+    Friend WithEvents BtnPrint As Button
+    Friend WithEvents BtnDelete As Button
+    Friend WithEvents BtnEdit As Button
+    Friend WithEvents PrtDocument As Printing.PrintDocument
+    Friend WithEvents PpdPreview As PrintPreviewDialog
+    Friend WithEvents BtnDeleteAll As Button
+    Friend WithEvents TxtLookup As TextBox
+    Friend WithEvents BtnLookup As Button
+End Class
---
+++ FamousHorses/FrmMain.vb
@@ -0,0 +1,296 @@
+﻿'Project            Chapter 10 part 1
+'Programmer         Theodore Feldman
+'Date               11/30/2023
+'Description        Uses an Access database to Add, Delete, Edit, Print, Save, and Search the database 
+'
+
+
+Public Class FrmMain
+
+    'Variables
+    Dim blnAdd As Boolean = False
+
+    Private Sub TblHorsesBindingNavigatorSaveItem_Click(sender As Object, e As EventArgs)
+        Me.Validate()
+        Me.TblHorsesBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.HorsesDataSet)
+
+    End Sub
+
+    Private Sub FrmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'HorsesDataSet.tblHorses' table. You can move, or remove it, as needed.
+        Me.TblHorsesTableAdapter.Fill(Me.HorsesDataSet.tblHorses)
+
+    End Sub
+
+    Private Sub BtnNext_Click(sender As Object, e As EventArgs) Handles BtnNext.Click
+
+        With TblHorsesBindingSource
+            If .Position = .Count - 1 Then
+                .MoveFirst()
+            Else
+                .MoveNext()
+            End If
+        End With
+    End Sub
+
+    Private Sub BtnPrevious_Click(sender As Object, e As EventArgs) Handles BtnPrevious.Click
+
+        With TblHorsesBindingSource
+            If .Position = 0 Then
+                .MoveLast()
+            Else
+                .MovePrevious()
+            End If
+        End With
+    End Sub
+
+    Private Sub BtnFirst_Click(sender As Object, e As EventArgs) Handles BtnFirst.Click
+        TblHorsesBindingSource.MoveFirst()
+    End Sub
+
+    Private Sub BtnLast_Click(sender As Object, e As EventArgs) Handles BtnLast.Click
+        TblHorsesBindingSource.MoveLast()
+    End Sub
+
+    Private Sub BtnAdd_Click(sender As Object, e As EventArgs) Handles BtnAdd.Click
+        'Adds new record or cancels the add
+
+        If BtnAdd.Text = "Add" Then
+            BtnAdd.Text = "Cancel"
+            BtnSave.Enabled = True
+            DisableButtons()
+            UnlockTextBoxes()
+            BtnEdit.Enabled = False
+            ClearTextBoxes()
+            blnAdd = True
+
+            TblHorsesBindingSource.EndEdit()
+            TblHorsesBindingSource.AddNew()
+        Else
+            BtnAdd.Text = "Add"
+            BtnSave.Enabled = False
+            EnableButtons()
+            LockTextBoxes()
+            BtnEdit.Enabled = True
+            blnAdd = False
+
+            TblHorsesTableAdapter.Fill(HorsesDataSet.tblHorses)
+        End If
+    End Sub
+
+    Private Sub DisableButtons()
+        BtnDelete.Enabled = False
+        BtnPrint.Enabled = False
+        BtnFirst.Enabled = False
+        BtnLast.Enabled = False
+        BtnPrevious.Enabled = False
+        BtnNext.Enabled = False
+    End Sub
+    Private Sub EnableButtons()
+        BtnDelete.Enabled = True
+        BtnPrint.Enabled = True
+        BtnSave.Enabled = False
+        BtnLast.Enabled = True
+        BtnPrevious.Enabled = True
+        BtnNext.Enabled = True
+        BtnFirst.Enabled = True
+    End Sub
+
+    Private Sub UnlockTextBoxes()
+        HorseTextBox.ReadOnly = False
+        RiderTextBox.ReadOnly = False
+        SeriesTextBox.ReadOnly = False
+    End Sub
+
+    Private Sub LockTextBoxes()
+        HorseTextBox.ReadOnly = True
+        RiderTextBox.ReadOnly = True
+        SeriesTextBox.ReadOnly = True
+    End Sub
+
+    Private Sub ClearTextBoxes()
+        HorseTextBox.Text = ""
+        HorseTextBox.Focus()
+        RiderTextBox.Text = ""
+        SeriesTextBox.Text = ""
+    End Sub
+
+    Private Sub BtnSave_Click(sender As Object, e As EventArgs) Handles BtnSave.Click
+
+        If blnAdd = True Then
+            Try
+                HorsesDataSet.AcceptChanges()
+                TblHorsesBindingSource.EndEdit()
+
+                TblHorsesTableAdapter.Update(HorsesDataSet)
+            Catch ex As Exception
+                MessageBox.Show(ex.Message)
+            End Try
+
+        Else
+            MessageBox.Show("Not available for adding.", "Add Error", MessageBoxButtons.OK)
+        End If
+
+        blnAdd = False
+        LockTextBoxes()
+        EnableButtons()
+
+        BtnAdd.Text = "&Add"
+        BtnEdit.Enabled = True
+        BtnSave.Enabled = False
+
+        'Refresh the database
+        TblHorsesTableAdapter.Fill(HorsesDataSet.tblHorses)
+    End Sub
+
+    Private Sub BtnEdit_Click(sender As Object, e As EventArgs) Handles BtnEdit.Click
+        'Edits the existing records
+        If BtnEdit.Text = "Edit" Then
+            BtnEdit.Text = "Update"
+            DisableButtons()
+            UnlockTextBoxes()
+            BtnAdd.Enabled = False
+            HorseTextBox.Focus()
+        Else
+            BtnEdit.Text = "Edit"
+            EnableButtons()
+            LockTextBoxes()
+            BtnAdd.Enabled = True
+
+            TblHorsesBindingSource.EndEdit()
+            TblHorsesTableAdapter.Update(HorsesDataSet.tblHorses)
+
+            TblHorsesTableAdapter.Fill(HorsesDataSet.tblHorses)
+        End If
+    End Sub
+
+    Private Sub BtnDelete_Click(sender As Object, e As EventArgs) Handles BtnDelete.Click
+        'Removes the Current active record
+        Dim intReply As Integer
+        intReply = MessageBox.Show("Delete the horse " & HorseTextBox.Text & "?", "Deletion Request", MessageBoxButtons.YesNo)
+        Try
+            If intReply = vbYes Then
+                TblHorsesBindingSource.RemoveCurrent()
+                TblHorsesTableAdapter.Update(HorsesDataSet.tblHorses)
+            Else
+                TblHorsesBindingSource.CancelEdit()
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Error deleting record")
+        End Try
+    End Sub
+
+    Private Sub PpdPreview_Load(sender As Object, e As EventArgs) Handles PpdPreview.Load
+        'handels the preview of the document
+    End Sub
+
+    Private Sub BtnPrint_Click(sender As Object, e As EventArgs) Handles BtnPrint.Click
+        'Prints the preview of the document
+        PpdPreview.Document = PrtDocument
+        PpdPreview.ShowDialog()
+    End Sub
+
+    Private Sub PrtDocument_PrintPage(sender As Object, e As Printing.PrintPageEventArgs) Handles PrtDocument.PrintPage
+        'Sets up the printout
+
+        Dim fntPrint As New Font("Arial", 12)
+        Dim fntHeading As New Font("Arial", 16, FontStyle.Bold)
+        Dim sngLineHeight As Single = fntPrint.GetHeight + 2
+        Dim sngPrintX As Single = e.MarginBounds.Left
+        Dim sngPrintY As Single = e.MarginBounds.Top
+        Dim strPrintLine As String
+        Dim intCount As Integer
+
+        strPrintLine = "Famous Horses"
+        e.Graphics.DrawString(strPrintLine, fntHeading, Brushes.DarkRed, sngPrintX, sngPrintY)
+
+        sngPrintY += sngLineHeight
+        strPrintLine = "by Theodore Feldman"
+        e.Graphics.DrawString(strPrintLine, fntHeading, Brushes.DarkRed, sngPrintX, sngPrintY)
+
+        sngPrintY += sngLineHeight * 2
+
+        TblHorsesBindingSource.MoveFirst()
+
+        For intCount = 0 To TblHorsesBindingSource.Count - 1
+            strPrintLine = HorseTextBox.Text.ToString()
+            e.Graphics.DrawString(strPrintLine, fntPrint, Brushes.Red, sngPrintX, sngPrintY)
+
+            sngPrintX += 200
+            strPrintLine = RiderTextBox.Text.ToString()
+            e.Graphics.DrawString(strPrintLine, fntPrint, Brushes.OrangeRed, sngPrintX, sngPrintY)
+
+            sngPrintX += 200
+            strPrintLine = SeriesTextBox.Text.ToString()
+            e.Graphics.DrawString(strPrintLine, fntPrint, Brushes.DarkMagenta, sngPrintX, sngPrintY)
+
+            sngPrintX = e.MarginBounds.Left
+            sngPrintY += sngLineHeight
+            TblHorsesBindingSource.MoveNext()
+
+        Next
+
+        sngPrintY += sngLineHeight * 3
+        strPrintLine = "*** End Of Report ***"
+        e.Graphics.DrawString(strPrintLine, fntPrint, Brushes.DarkRed, sngPrintX, sngPrintY)
+    End Sub
+
+    Private Sub BtnDeleteAll_Click(sender As Object, e As EventArgs) Handles BtnDeleteAll.Click
+        'Deletes all of the records in the database table
+
+        Dim intReply As Integer
+        Dim intCount As Integer
+        Dim intDeleteAll As Integer
+
+        intCount = TblHorsesBindingSource.Count - 1
+
+        intReply = MessageBox.Show("Delete All Records?", "Delete All", MessageBoxButtons.YesNo)
+        If intReply = vbYes Then
+            Try
+                Do While intDeleteAll <= intCount
+                    HorsesDataSet.tblHorses.Rows(0).Delete()
+                    TblHorsesTableAdapter.Update(HorsesDataSet.tblHorses)
+                    intDeleteAll += 1
+                Loop
+                TblHorsesTableAdapter.Fill(HorsesDataSet.tblHorses)
+            Catch ex As Exception
+                MessageBox.Show("Unable to delete the records.", "Horses", MessageBoxButtons.OK)
+            End Try
+
+        Else
+            MessageBox.Show("Deleteion Canceled", "Horses")
+        End If
+    End Sub
+
+    Private Sub BtnLookup_Click(sender As Object, e As EventArgs) Handles BtnLookup.Click
+        'Searches the database table for a specific horse name
+
+        Dim strHorse As String
+        Dim intCount As Integer
+        Dim blnFound As Boolean = False
+        Dim intPosition As Integer
+
+        strHorse = CStr(TxtLookup.Text)
+        intCount = TblHorsesBindingSource.Count
+
+        TblHorsesBindingSource.MoveFirst()
+
+        Do Until blnFound = True Or intPosition >= intCount
+            If UCase(strHorse) = UCase(HorseTextBox.Text) Then
+                blnFound = True
+            Else
+                TblHorsesBindingSource.MoveNext()
+            End If
+            intPosition += 1
+        Loop
+
+        If blnFound = False Then
+            TblHorsesBindingSource.MoveFirst()
+            TxtLookup.Text = "Not Found"
+        Else
+            TxtLookup.Text = ""
+        End If
+
+    End Sub
+End Class
